51bdc73708c0d3f10760143b16ee6298a8ae0322
Kattoor <jasper.catthoor@gmail.com>
2021-12-08 19:27:55 +0100

    Refactored day 8 util file

--- solutions/day_8/bin/util.dart
+++ solutions/day_8/bin/util.dart
@@ -30,99 +30,75 @@ Future<List<Record>> getRecords() async {
   return inputLines.map((line) => Record.fromLine(line)).toList();
 }
 
-List<int> convert(Record record, List<Map<String, String>> possibilities,
-    Map<int, String> numbers) {
-  List<SignalPattern> signalPatterns = record.signalPatterns;
-  List<SignalPattern> outputValues = record.outputValues;
+bool isValid(List<String> changedSignalPatterns, String number) {
+  final indexOfNumber = changedSignalPatterns.indexOf(number);
+  if (indexOfNumber == -1) {
+    return false;
+  }
+  changedSignalPatterns.removeAt(indexOfNumber);
+  return true;
+}
 
-  for (var possibility in possibilities) {
-    final changedSignalPatterns = signalPatterns.map((pattern) {
-      final changedSignals =
-          pattern.signal.map((char) => possibility[char]).toList();
-      changedSignals.sort();
-      return changedSignals.join();
-    }).toList();
+List<String> mapSignalPatterns(
+  List<SignalPattern> originalSignalPatterns,
+  Map<String, String> mapping,
+) {
+  return originalSignalPatterns.map((pattern) {
+    final changedSignals = pattern.signal.map((char) => mapping[char]).toList();
+    changedSignals.sort();
+    return changedSignals.join();
+  }).toList();
+}
 
-    final indexOfZero = changedSignalPatterns.indexOf(numbers[0]!);
-    if (indexOfZero == -1) {
-      continue;
+bool isCorrectMapping(
+  Record record,
+  Map<String, String> possibility,
+  Map<int, String> numbers,
+) {
+  final mappedSignalPatterns =
+      mapSignalPatterns(record.signalPatterns, possibility);
+
+  for (var i = 0; i < 10; i++) {
+    if (!isValid(mappedSignalPatterns, numbers[i]!)) {
+      return false;
     }
-    changedSignalPatterns.removeAt(indexOfZero);
+  }
 
-    final indexOfOne = changedSignalPatterns.indexOf(numbers[1]!);
-    if (indexOfOne == -1) {
-      continue;
-    }
-    changedSignalPatterns.removeAt(indexOfOne);
+  return true;
+}
 
-    final indexOfTwo = changedSignalPatterns.indexOf(numbers[2]!);
-    if (indexOfTwo == -1) {
-      continue;
-    }
-    changedSignalPatterns.removeAt(indexOfTwo);
+List<int> getMappedOutputValues(
+    Record record, Map<String, String> possibility, Map<int, String> numbers) {
+  final mappedSignalPatterns =
+      mapSignalPatterns(record.outputValues, possibility);
 
-    final indexOfThree = changedSignalPatterns.indexOf(numbers[3]!);
-    if (indexOfThree == -1) {
-      continue;
-    }
-    changedSignalPatterns.removeAt(indexOfThree);
-
-    final indexOfFour = changedSignalPatterns.indexOf(numbers[4]!);
-    if (indexOfFour == -1) {
-      continue;
-    }
-    changedSignalPatterns.removeAt(indexOfFour);
-
-    final indexOfFive = changedSignalPatterns.indexOf(numbers[5]!);
-    if (indexOfFive == -1) {
-      continue;
-    }
-    changedSignalPatterns.removeAt(indexOfFive);
-
-    final indexOfSix = changedSignalPatterns.indexOf(numbers[6]!);
-    if (indexOfSix == -1) {
-      continue;
-    }
-    changedSignalPatterns.removeAt(indexOfSix);
-
-    final indexOfSeven = changedSignalPatterns.indexOf(numbers[7]!);
-    if (indexOfSeven == -1) {
-      continue;
-    }
-    changedSignalPatterns.removeAt(indexOfSeven);
-
-    final indexOfEight = changedSignalPatterns.indexOf(numbers[8]!);
-    if (indexOfEight == -1) {
-      continue;
-    }
-    changedSignalPatterns.removeAt(indexOfEight);
-
-    final indexOfNine = changedSignalPatterns.indexOf(numbers[9]!);
-    if (indexOfNine == -1) {
-      continue;
-    }
-    changedSignalPatterns.removeAt(indexOfNine);
-
-    final changedOutputValues = outputValues.map((pattern) {
-      final changedSignals =
-          pattern.signal.map((char) => possibility[char]).toList();
-      changedSignals.sort();
-      return changedSignals.join();
-    }).toList();
-
-    return changedOutputValues.map((signal) {
-      for (var i = 0; i < numbers.length; i++) {
-        if (signal == numbers[i]) {
-          return i;
-        }
+  return mappedSignalPatterns.map((signal) {
+    for (var i = 0; i < numbers.length; i++) {
+      if (signal == numbers[i]) {
+        return i;
       }
-      return -1;
-    }).toList();
+    }
+    return -1;
+  }).toList();
+}
+
+List<int> convert(
+  Record record,
+  List<Map<String, String>> possibilities,
+  Map<int, String> numbers,
+) {
+  for (var possibility in possibilities) {
+    if (!isCorrectMapping(record, possibility, numbers)) {
+      continue;
+    }
+
+    return getMappedOutputValues(record, possibility, numbers);
   }
 
   return [];
 }
 
+/* Get a list of all possible signal pattern mappings */
 List<Map<String, String>> permute(List<Position> defaultPositions) {
   List<Map<String, String>> results = [];
 
@@ -130,8 +106,10 @@ List<Map<String, String>> permute(List<Position> defaultPositions) {
     if (positions.isEmpty) {
       results.add(Map.fromEntries([
         for (var i = 0; i < defaultPositions.length; i += 1)
-          MapEntry<String, String>(defaultPositions[i].toString().split('.')[1],
-              m[i].toString().split('.')[1])
+          MapEntry(
+            defaultPositions[i].toString().split('.')[1],
+            m[i].toString().split('.')[1],
+          )
       ]));
     } else {
       for (var i = 0; i < positions.length; i++) {
